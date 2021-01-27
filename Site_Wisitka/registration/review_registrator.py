8a8bda6340fb1031e367d26eae38286f5f1aaaf5
lateir <lateir@gmail.com>
2021-01-27 22:10:52 +0500

    Добавлен вывод комментариев

--- Site_Wisitka/registration/review_registrator.py
+++ Site_Wisitka/registration/review_registrator.py
@@ -8,10 +8,15 @@ def register_review(request):
     review_db = ReviewDb(
         username=request.params['username'],
         comment=request.params['comment'],
-        date=datetime.now(Tz=None)
+        date=datetime.now(tz=None)
     )
     request.dbsession.add(review_db)
     transaction.commit()
 
     query = request.dbsession.query(ReviewDb)
     return query.order_by(ReviewDb.id.desc()).first()
+
+
+def get_all_reviews(request):
+    query = request.dbsession.query(ReviewDb)
+    return query.order_by(ReviewDb.id)
--- Site_Wisitka/views/reviewer.py
+++ Site_Wisitka/views/reviewer.py
@@ -1,18 +1,18 @@
 from pyramid.view import view_config
-from Site_Wisitka.registration.review_registrator import register_review
+from Site_Wisitka.registration.review_registrator import register_review, get_all_reviews
 from ..registration.feedback_registrator import register_feedback
 
 
-@view_config(route_name='home', renderer='../templates/mytemplate.jinja2')
+@view_config(route_name='home', renderer='../templates/index.jinja2')
 def reviewer(request):
-    review = None
     feedback = None
 
     if request.params.__contains__('review'):
-        review = register_review(request)
+        register_review(request)
     elif request.params.__contains__('feedback'):
         feedback = register_feedback()
 
+    reviews = get_all_reviews(request)
     return {'project': 'Site_Wisitka',
-            'review': review,
+            'reviews': reviews,
             'feedback': feedback}
